fcbf4775d886c830c003522ca770cb3485233fc9
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-03-13 20:50:07 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,29 @@
+package eva2_5_tipos_herencia;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA2_5_TIPOS_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    class SuperClase{
+    
+        
+    }
+    
+    class OtraSuperClase{ //NO SE PUEDE HEREDAR DE DOS O MAS SUPERCLASES
+    
+    }
+                                //extends solo funciona para una unica clase
+    class BaseClase extends SuperClase{
+
+}
+    
+}
