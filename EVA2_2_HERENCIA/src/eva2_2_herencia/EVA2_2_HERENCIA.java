edec5683ac487161659f0d34b3a0b616ff558e5e
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-03-13 19:54:53 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/EVA2_2_HERENCIA.java
@@ -0,0 +1,18 @@
+package eva2_2_herencia;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA2_2_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+        
+    }
+    
+}
---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/Estudiante.java
@@ -0,0 +1,49 @@
+package eva2_2_herencia;
+
+/**
+ *
+ * @author danna
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String nControl;
+
+    public Estudiante() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getnControl() {
+        return nControl;
+    }
+
+    public void setnControl(String nControl) {
+        this.nControl = nControl;
+    }
+    
+    
+}
